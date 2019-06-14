0259171b4ba8d3388b5d815edfad9586a4e7ad8b
Cristian Araujo <araujocristian@live.com>
2019-06-13 23:12:51 -0300

    create  new components

--- src/home/containers/Home.js
+++ src/home/containers/Home.js
@@ -5,6 +5,10 @@ import { connect } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 //Helpers
 import { translateUnit } from "../../common";
+//Components
+import Unit from "../components/Unit";
+import Menu from "../components/Menu";
+
 // Actions
 import { getUnits, getUnit } from "../actions";
 // Selectors
@@ -14,9 +18,6 @@ import {
   currentUnitSelector,
   currentMenuItemsSelector
 } from "../selectors";
-import recipes from "../../services/recipes.json";
-import RecipeItem from "../components/RecipeItem";
-
 //Style
 import "./Home.css";
 
@@ -24,7 +25,6 @@ export class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.recipes = recipes.results;
     this.state = {
       unitName: "--"
     };
@@ -42,15 +42,14 @@ export class Home extends Component {
   render() {
     return (
       <UnauthenticatedRedirect>
-        {console.log(this.props, "STATE")}
-        <div>
+        <>
           <div className="unidade-seletor">
             <span>Selecione sua unidade:</span>
             <select
               onChange={e => this.handleGetMeal(e.target.value)}
               defaultValue={this.state.unitName}
             >
-              <option selected>Escolher...</option>
+              <option value="--">Escolher...</option>
               {this.props.unitNameList.map((unit, i) => {
                 return (
                   <option key={i} value={unit}>
@@ -62,56 +61,18 @@ export class Home extends Component {
           </div>
           {this.props.currentUnit.length !== 0 ? (
             <>
-              <div className="unidade">
-                <span className="unidade-nome">{`Unidade: ${translateUnit(
-                  this.props.currentUnit.name
-                )}`}</span>
-                <span>{`Endereço: ${this.props.currentUnit.location}`}</span>
-                <span>
-                  {`Preço: ${parseFloat(
-                    this.props.currentUnit.priceCharged
-                  ).toLocaleString("pt-br", {
-                    style: "currency",
-                    currency: "BRL"
-                  })}`}
-                </span>
-              </div>
-              <div className="cardapio">
-                <div className="cardapio-nome">
-                  <p>Cardapio do dia</p>
-                </div>
-                {this.props.currentUnit.currentMenu.menuItems.length ? (
-                  <>
-                    <div className="refeicoes">
-                      {this.props.currentMenuItems.map((recipe, ind) => (
-                        <RecipeItem
-                          key={ind}
-                          ingredients={recipe.foodItem.description}
-                          thumbnail={recipe.foodItem.icon}
-                          title={recipe.foodItem.name}
-                          vegetarian={recipe.foodItem.vegetarian}
-                          vegan={recipe.foodItem.vegan}
-                        />
-                      ))}
-                    </div>
-                    <div className="fila">
-                      <span>Status da fila:</span>
-                      <span>{this.props.currentUnit.queueStatus}</span>
-                    </div>
-                  </>
-                ) : (
-                  <div className="sem-refeicoes">
-                    <h1>
-                      Nenhum cardápio cadastrado, faça a sua contribuição!
-                    </h1>
-                  </div>
-                )}
-              </div>
+              <Unit currentUnit={this.props.currentUnit} />
+              <Menu 
+                currentUnit={this.props.currentUnit}
+                currentMenuItems={this.props.currentMenuItems}
+              />
               <div className="new-refeicao">
                 <span>Adicione aqui sua contribuição!</span>
-                <Link to="/form"><button className="btn btn-deep-orange">
-                  Adicionar Refeição
-                </button></Link>
+                <Link to="/form">
+                  <button className="btn btn-deep-orange">
+                    Adicionar Refeição
+                  </button>
+                </Link>
               </div>
             </>
           ) : (
@@ -119,7 +80,7 @@ export class Home extends Component {
               <h1>Escolha uma unidade.</h1>
             </div>
           )}
-        </div>
+        </>
       </UnauthenticatedRedirect>
     );
   }
